a9daee5e706988606b3aff876245d9e3cc86ebfd
Carlos Zanotti <cezanotti@gmail.com>
2025-09-24 16:28:32 -0300

    Adds modal functionality

---
+++ script.js
@@ -0,0 +1,17 @@
+let modal = document.getElementById("joke-modal");
+let btn = document.getElementById("get-joke");
+let span = document.getElementsByClassName("modal__close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}

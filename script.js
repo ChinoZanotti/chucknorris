fb58e93c7adf627fb0c2cb1f26ae3c8ab75b815f
Carlos Zanotti <cezanotti@gmail.com>
2025-09-24 18:03:24 -0300

    Adds a favicon and a loading animation

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ let btn = document.getElementById("get-joke");
 let span = document.getElementsByClassName("modal__close")[0];
 
 btn.onclick = function() {
+    document.getElementById("joke").innerHTML = `<span class="loading">👊</span>`;
     modal.style.display = "block";
+    // getJoke("https://api.chucknorris.io/jokes/random");
 }
 
 span.onclick = function() {
@@ -14,4 +16,16 @@ window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}
+
+//Chuck Norris Jokes API
+async function getJoke(url) {
+  try {
+    const response = await fetch(url);
+    const jokeObject = await response.json();
+    const joke = jokeObject.value;
+    document.getElementById("joke").innerHTML = joke;
+  } catch (err) {
+    console.error("Error connecting to the API: ", err);
+  }
 }
